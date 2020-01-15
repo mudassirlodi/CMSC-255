ec42a67d1d3fc27202d05a2dcef6c5d83774118c
mudassirlodi <59939899+mudassirlodi@users.noreply.github.com>
2020-01-15 17:33:19 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Mudassir Lodi");
+		System.out.println("Lab 1");
+		System.out.println("Hello World");
+		System.out.println("January 15, 2020");
+		System.out.println("CMSC 255, Section 01");
+		}
+}
